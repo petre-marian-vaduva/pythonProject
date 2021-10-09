2b13aca99a2b5f894197bc335f7872c454ce0756
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-10-09 22:11:48 +0100

    Practice with OOP

--- war game.py
+++ war game.py
@@ -1,15 +1,27 @@
 import random
-suits = {'Hearts', 'Diamonds', 'Spades', 'clubs'}
-ranks = {'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten','Jack', 'Queen', 'Kind', 'Ace'}
+suits = {'Hearts', 'Diamonds', 'Spades', 'Clubs'}
+ranks = {'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'Kind', 'Ace'}
 values = {
     'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
     'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14
 }
 
-class Card():
+class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
         self.values = values[rank]
+
     def __str__(self):
         return self.rank + 'of' + self.suit
+
+
+class Deck:
+    def __init__(self):
+        self.all_cards = []
+        for suit in suits:
+            for rank in ranks:
+                created_card = Card(suit, rank)
+                self.all_cards.append(created_card)
+
+
