91de610cd15420cd830a3d4890eb6e9b1ee86e0c
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-10-15 21:58:59 +0100

    algorithm 2

--- alghoritms.py
+++ alghoritms.py
@@ -24,3 +24,35 @@ def consecutive(arr):
         if i not in arr:
             count += 1
     return count
+
+# Challenge:
+#
+# Given a two-dimensional array, return a new array which carries over only those arrays from the original,
+# which were not empty and whose items are all of the same type (i.e. homogenous).
+# For simplicity, the arrays inside the array will only contain characters and integers.
+# Example:
+#
+# Given [[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]], your function should return [[1, 5, 4], ['b']].
+#
+# Please keep in mind that for this kata, we assume that empty arrays are not homogenous.
+#
+# The resultant arrays should be in the order they were originally in and should not have its values changed.
+
+# No implicit type casting is allowed. A subarray [1, '2'] would be considered illegal and should be filtered out.
+
+def filter_homogenous(arrays):
+    new_arr = []
+    for i in arrays:
+        if not i:
+            continue
+        else:
+            is_int = True
+            is_str = True
+            for j in i:
+                if type(j) != int:
+                    is_int = False
+                elif type(j) != str:
+                    is_str = False
+            if is_int or is_str:
+                new_arr.append(i)
+    return new_arr
