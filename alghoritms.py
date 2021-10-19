598f96bd2a476baf27bcfcd6f588389ec1e70ea8
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-10-19 23:07:50 +0100

    practice class inheritance

--- alghoritms.py
+++ alghoritms.py
@@ -220,43 +220,58 @@
 #add_student student
 # get_average_grade
 
-class Student:
-    def __init__(self, name, age, grade):
+# class Student:
+#     def __init__(self, name, age, grade):
+#         self.name = name
+#         self.age = age
+#         self.grade = grade
+#
+#     def get_grade(self):
+#         return self.grade
+#
+# class Course:
+#     def __init__(self, name, max_students):
+#         self.name = name
+#         self.max_students = max_students
+#         self.students = []
+#
+#     def add_student(self, student):
+#         if len(self.students) < self.max_students:
+#             self.students.append(student)
+#             return True
+#         return False
+#
+#     def get_average_grade(self):
+#         value = 0
+#         for student in self.students:
+#             value += student.get_grade()
+#         return value / len(self.students)
+#
+# s1 = Student('Tim', 18, 80)
+# s2 = Student('Bob', 18, 190)
+# s3 = Student('Tech', 20, 40)
+#
+# course = Course('Geography', 2)
+# course.add_student(s1)
+# course.add_student(s2)
+# course.add_student(s3)
+# print(course.get_average_grade())
+
+class Pet:
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.grade = grade
 
-    def get_grade(self):
-        return self.grade
+    def show(self):
+        print(f'I am {self.name} and I am {self.age} years old')
 
-class Course:
-    def __init__(self, name, max_students):
-        self.name = name
-        self.max_students = max_students
-        self.students = []
+    def speak(self):
+        print('I don\'t know what to say')
 
-    def add_student(self, student):
-        if len(self.students) < self.max_students:
-            self.students.append(student)
-            return True
-        return False
-
-    def get_average_grade(self):
-        value = 0
-        for student in self.students:
-            value += student.get_grade()
-        return value / len(self.students)
-
-s1 = Student('Tim', 18, 80)
-s2 = Student('Bob', 18, 190)
-s3 = Student('Tech', 20, 40)
-
-course = Course('Geography', 2)
-course.add_student(s1)
-course.add_student(s2)
-course.add_student(s3)
-print(course.get_average_grade())
-
+class Cat(Pet):
+    def __init__(self, name, age, color):
+        super().__init__(name, age)
+        self.color = color
 
 
 
