e90025fc5701e9ecc2d54bdf7adb8454f22dedc9
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-10-20 20:49:40 +0100

    List Comprehension practice

--- alghoritms.py
+++ alghoritms.py
@@ -257,21 +257,29 @@
 # course.add_student(s3)
 # print(course.get_average_grade())
 
-class Pet:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Pet:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def show(self):
+#         print(f'I am {self.name} and I am {self.age} years old')
+#
+#     def speak(self):
+#         print('I don\'t know what to say')
+#
+# class Cat(Pet):
+#     def __init__(self, name, age, color):
+#         super().__init__(name, age)
+#         self.color = color
 
-    def show(self):
-        print(f'I am {self.name} and I am {self.age} years old')
 
-    def speak(self):
-        print('I don\'t know what to say')
+arr = [i for i in range(10)]
 
-class Cat(Pet):
-    def __init__(self, name, age, color):
-        super().__init__(name, age)
-        self.color = color
+
+
+
+
 
 
 
