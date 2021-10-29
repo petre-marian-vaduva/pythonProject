b0d228d5b441b378784f91ded1f45b31f187a18d
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-10-29 05:32:47 +0100

    algorithm 13

--- alghoritms.py
+++ alghoritms.py
@@ -733,7 +733,18 @@
 #                 return False
 #     return True
 
+# Rules are: (1) the letters are adjacent in the English alphabet, and (2) each letter occurs only once.
+#
+# For example:
+# solve("abc") = True, because it contains a,b,c
+# solve("abd") = False, because a, b, d are not consecutive/adjacent in the alphabet, and c is missing.
+# solve("dabc") = True, because it contains a, b, c, d
+# solve("abbc") = False, because b does not occur once.
+# solve("v") = True
 
+# from string import ascii_lowercase
+# def solve(st):
+#     return ''.join(sorted(st.lower())) in ascii_lowercase
 
 
 
