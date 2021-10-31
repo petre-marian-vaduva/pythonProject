48eeaf05c5c9ca89aef2ee634c3125f4ab6b6c79
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-10-31 01:39:59 +0000

    algorithm 15

--- alghoritms.py
+++ alghoritms.py
@@ -768,3 +768,23 @@
 #     for i in range(count):
 #         new_s += '!'
 #     return new_s
+
+
+
+# Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+#
+# Write a function which takes a list of strings and returns each line prepended by the correct number.
+#
+# The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+#
+# Examples:
+#
+# number([]) # => []
+# number(["a", "b", "c"]) # => ["1: a", "2: b", "3: c"]
+
+# def number(lines):
+#     lst = []
+#     for i in range(len(lines)):
+#         lst.append(f'{i+1}: {lines[i]}')
+#     return lst
+
