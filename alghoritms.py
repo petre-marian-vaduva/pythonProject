04b9368a47c2d507a0685c2a408414b53f6848b1
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-10-31 05:06:03 +0000

    Practiced reversed with lists

--- alghoritms.py
+++ alghoritms.py
@@ -813,3 +813,21 @@
 #     return new_s
 #
 # print(remove('!Hi'))
+
+
+# Given an array of digitals numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+#
+# For example:
+#
+# ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+# ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+# ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+#
+# def even_numbers(arr,n):
+#     new_arr = []
+#     for num in reversed(arr):
+#         if num % 2 == 0:
+#             new_arr.append(num)
+#         if len(new_arr) == n:
+#             break
+#     return list(reversed(new_arr))
