b11106a65af0eba731bc895bdedc3dc8ae72669f
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-11-01 21:25:14 +0000

    Practiced upper() and lower() functions

--- alghoritms.py
+++ alghoritms.py
@@ -845,3 +845,33 @@
 #         if l > count:
 #             l = count
 #     return l
+
+
+
+
+
+# In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+#
+# make as few changes as possible.
+# if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+# For example:
+#
+# solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+# solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+# solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+# def solve(s):
+#     count_lower = 0
+#     count_upper = 0
+#     for char in s:
+#         if char.islower():
+#             count_lower += 1
+#         else:
+#             count_upper += 1
+#     if count_lower > count_upper or count_lower == count_upper:
+#         return s.lower()
+#     elif count_upper > count_lower:
+#         return s.upper()
+
+
+
