bacf64f277004e86b253c32986d096a4bca0f8d7
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-11-04 06:39:26 +0000

    creating and adding elements to table

--- my_sql.py
+++ my_sql.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from datetime import datetime
 
 db = mysql.connector.connect(
     host = 'localhost',
@@ -9,9 +10,7 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-# mycursor.execute('CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)')
-mycursor.execute('INSERT INTO Person (name, age) VALUES (%s,%s)', ('Tim', 19))
-db.commit()
+# mycursor.execute("CREATE TABLE Testing (name varchar(50) NOT NULL, created datetime NOT NULL, gender ENUM('M', 'F', 'O') NOT NULL, id int PRIMARY KEY NOT NULL AUTO_INCREMENT)")
+# mycursor.execute('INSERT INTO Testing (name, created, gender) VALUES (%s, %s, %s)', ('Marie', datetime.now(), 'F'))
 
-# mycursor.execute()
 
