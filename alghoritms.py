245e92882b1476577a1283374229540548933479
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-11-06 06:28:52 +0000

    title() method

--- alghoritms.py
+++ alghoritms.py
@@ -887,9 +887,15 @@
 #             max_vowels = count
 #     return max_vowels
 
-
-
-
+# Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+#
+# Example:
+#
+# "riley" --> "Hello Riley!"
+# "JACK"  --> "Hello Jack!"
+#
+# def greet(name):
+#     return f'Hello {name.title()}!'
 
 
 
--- my_sql.py
+++ my_sql.py
@@ -13,7 +13,7 @@ mycursor = db.cursor()
 # mycursor.execute("CREATE TABLE Testing (name varchar(50) NOT NULL, created datetime NOT NULL, gender ENUM('M', 'F', 'O') NOT NULL, id int PRIMARY KEY NOT NULL AUTO_INCREMENT)")
 # mycursor.execute('INSERT INTO Testing (name, created, gender) VALUES (%s, %s, %s)', ('Mar', datetime.now(), 'F'))
 
-mycursor.execute('ALTER TABLE Testing CHANGE name first_name VARCHAR(50)')
+mycursor.execute('ALTER TABLE Testing CHANGE first_name first_name VARCHAR(50)')
 
 mycursor.execute('DESCRIBE Testing')
 for x in mycursor:
