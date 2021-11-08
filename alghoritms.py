52b5241a388c146b0d99ce7dca0611704694e000
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-11-08 06:07:50 +0000

    aray indexing

--- alghoritms.py
+++ alghoritms.py
@@ -958,9 +958,19 @@
 #     return True
 
 
+# Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
+#
+# Take a look on the test cases.
+#
+# Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
+#
+# The second value in the first integer array is 0, since the bus is empty in the first bus stop.
 
-
-
+# def number(bus_stops):
+#     total = 0
+#     for passengers in bus_stops:
+#         total += passengers[0] - passengers[1]
+#     return total
 
 
 
