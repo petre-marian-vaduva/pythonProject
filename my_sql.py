2b39338582dc8da20f992fb9a049b786d81a847d
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-11-20 11:35:26 +0000

    practice with FOREIGN KEYS & Relating Tables

--- my_sql.py
+++ my_sql.py
@@ -8,9 +8,9 @@ db = mysql.connector.connect(
     database = 'testdatabase'
 )
 
-users = [('tim', 'tech', '1234', 'tim@gmail.com'),
-         ('joe', 'joe123', '2222', 'joe@gmail.com'),
-         ('sarah', 'sarah123', 'pass123', 'sarah@gmail.com')]
+users = [('tim', 'tech'),
+         ('joe', 'joe123'),
+         ('sarah', 'sarah123')]
 
 users_scores = [(45,100),
                 (30,200),
@@ -19,13 +19,29 @@ users_scores = [(45,100),
 mycursor = db.cursor()
 
 
-Q1 = 'CREATE TABLE Userss (id int PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), passwd VARCHAR(50))'
-Q2 = 'CREATE TABLE Scoress (userID int PRIMARY KEY, FOREIGN KEY(userId) REFERENCES Users(id), game1 int DEFAULT 0, game2 int DEFAULT 0)'
+Q1 = 'CREATE TABLE Users (id int PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), passwd VARCHAR(50))'
+Q2 = 'CREATE TABLE Scores (userID int PRIMARY KEY, FOREIGN KEY(userId) REFERENCES Users(id), game1 int DEFAULT 0, game2 int DEFAULT 0)'
 
-mycursor.execute('SHOW TABLES')
+Q3 = 'INSERT INTO Users (name, passwd) VALUES (%s, %s)'
+Q4 = 'INSERT INTO Scores (userID, game1, game2) VALUES (%s, %s, %s)'
 
+for x, user in enumerate(users):
+    mycursor.execute(Q3, user)
+    last_id = mycursor.lastrowid
+    mycursor.execute(Q4, (last_id,) + users_scores[x])
+
+db.commit()
+
+mycursor.execute('SELECT * FROM Scores')
 for x in mycursor:
     print(x)
+
+mycursor.execute('SELECT * FROM Scores')
+for x in mycursor:
+    print(x)
+
+
+
 
 
 
